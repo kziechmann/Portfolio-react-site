05dc2bcb98f8a5978804ef44f345c226f2acb2eb
kziechmann <kziechmann@gmail.com>
2019-02-18 22:50:22 -0700

    About page started.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import Background from './components/Parallax_BG';
+import ParaBackground from './components/Parallax_BG';
+import About from './pages/About';
 import RBNavbar from './components/Navbar';
+import { Parallax , Background } from 'react-parallax';
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <header className="sticky-nav">
-          <RBNavbar></RBNavbar>
-        </header>
         <div className="App">
-          <Background></Background>
+          <nav className="sticky-nav">
+            <RBNavbar></RBNavbar>
+          </nav>
+          <div className="parallax">
+            <div className="row justify-content-center">
+              <h1> About </h1>
+            </div>
+            <div className="parallax_group">
+              <About/>
+              <ParaBackground/>
+            </div>
+            
+          </div>
         </div>
-      </React.Fragment>
     );
   }
 }
