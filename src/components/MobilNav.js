92b29aaeda8be192cf7b0f111c51b5f98cdf13b6
kziechmann <kziechmann@gmail.com>
2019-02-25 13:51:38 -0700

    mobile responsive navbar and other css working.

--- src/components/MobilNav.js
+++ src/components/MobilNav.js
@@ -22,16 +22,22 @@ class MobNavbar extends Component {
     render() {
       return (
         <Navbar bg="dark" expand="lg" variant="pills dark" className="transparent ">
+            <div className="container">
+            <div className="row">
+            <div className="col-12">
+            
             <LinkContainer to={`/${this.state.currentPage !== 0? this.state.pages[this.state.currentPage-1]: 'about'}` }>
-                <Nav.Item className="mr-2 ml-2 navIcon btn" onClick={()=>{this.pageBack()}}><img className="mr-1 ml-1 " src="./images/fontawesome-free-5.7.2-web/svgs/regular/caret-square-left.svg" width="20"alt="twitter icon"></img></Nav.Item>
+                <Nav.Item className="mr-2 ml-2 navIcon btn" onClick={()=>{this.pageBack()}}><img className="" src="./images/fontawesome-free-5.7.2-web/svgs/regular/caret-square-left.svg" width="20"alt="<"></img></Nav.Item>
               </LinkContainer>
             <Navbar.Brand>
                 <h3>Kevin Ziechmann</h3>
             </Navbar.Brand>
             <LinkContainer to={`/${this.state.currentPage !== 0? this.state.pages[this.state.currentPage]: 'websites'}` }>
-                <Nav.Item className="mr-2 ml-2 navIcon btn" onClick={()=>{this.pageForward()}}><img className="mr-1 ml-1 " src="./images/fontawesome-free-5.7.2-web/svgs/regular/caret-square-right.svg" width="20"alt="twitter icon"></img></Nav.Item>
+                <Nav.Item className="mr-2 ml-2 navIcon btn" onClick={()=>{this.pageForward()}}><img className="" src="./images/fontawesome-free-5.7.2-web/svgs/regular/caret-square-right.svg" width="20"alt=">"></img></Nav.Item>
               </LinkContainer>
-    
+            </div>
+            </div>
+            </div>
       </Navbar>
       );
     }
